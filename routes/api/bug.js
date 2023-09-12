cd828f918fde71cc8d1d7335f7a4bdd807801bfe
atticus-bute <Atticus_Bute@insideranken.org>
2023-09-12 11:49:41 -0500

    9/12

--- routes/api/bug.js
+++ routes/api/bug.js
@@ -5,9 +5,13 @@ import debug from 'debug';
 import e from 'express';
 const debugBug = debug('app:BugRouter');
 
+import { nanoid } from 'nanoid';
+
 router.use(express.urlencoded({ extended: false }));
 
-const bugsArray = [];
+const bugsArray = [
+  {title:'example', description:'lorem ipsum...', stepsToReproduce:'lorem ipsum...', bugId:1, creationTime:Date()}
+];
 
 router.get('/list', (req, res) => {
   debugBug('bug list route hit');
@@ -16,11 +20,27 @@ router.get('/list', (req, res) => {
 
 router.get('/:bugId', (req, res) => {
   const bugId = req.params.bugId;
+  const bug = bugsArray.find((bug) => bug.bugId == bugId);
   //FIXME: get bug from bugsArray and send response as JSON
+  if (bug) {
+    res.status(200).json(bug);
+  } else {
+    res.status(404).json({ error: `Bug id #${bugId} not found` });
+  }
 });
 
 router.post('/new', (req, res) => {
   //FIXME: create new bug and send response as JSON
+  debugBug('hit new');
+  const newBug = req.body;
+  if (newBug.title && newBug.description && newBug.stepsToReproduce) {
+    newBug.bugId = nanoid();
+    newBug.creationTime = Date();
+    bugsArray.push(newBug);
+    res.status(200).json({message: `New bug ${newBug.title} reported!`});
+  } else {
+    res.status(400).json({error: 'Missing fields required.'});
+  }
 });
 
 router.put('/:bugId', (req, res) => {
--- routes/api/user.js
+++ routes/api/user.js
@@ -4,15 +4,18 @@ const router = express.Router();
 import debug from 'debug';
 const debugUser = debug('app:UserRouter');
 
+import { nanoid } from 'nanoid';
+
 router.use(express.urlencoded({ extended: false }));
 
 //FIXME: use this array to store user data in for now
 //we will replace this with a database in a later assignment
 const usersArray = [
-  { name: 'Atticus', userId: 1 },
-  { name: 'Evan', userId: 2 },
-  { name: 'Shion', userId: 3 },
-  { name: 'Samus', userId: 4 },
+  { email: 'jagbra@mail.com', password: 'crss', fullName: 'Atticus Bute', givenName:'Atticus', familyName: 'Bute', role:'Admin', userId: 1, creationTime: Date() },
+  { email: 'eagud@mail.com', password: 'eagud', fullName: 'Evan Gudmestad', givenName:'Evan', familyName: 'Gudmestad', role:'Admin', userId: nanoid(), creationTime: Date() },
+  { email: 'metroandroid@mail.com', password: 'morphball', fullName: 'Samus Aran', givenName:'Samus', familyName: 'Aran', role:'User', userId: nanoid(), creationTime: Date() },
+  { email: 'vector@mail.com', password: 'yeshallbeasgods', fullName: 'Shion Uzuki', givenName:'Shion', familyName: 'Uzuki', role:'User', userId: nanoid(), creationTime: Date() },
+  { email: 'halberd@mail.com', password: 'galaxia', fullName: 'Meta Knight', givenName:'Meta', familyName: 'Knight', role:'User', userId: nanoid(), creationTime: Date() }
 ];
 
 router.get('/list', (req, res) => {
@@ -21,6 +24,7 @@ router.get('/list', (req, res) => {
 });
 
 router.get('/:userId', (req, res) => {
+  debugUser('getting user by id');
   //reads the userId from the URL and stores it in a variable
   const userId = req.params.userId;
   //get the user from usersArray and send response as JSON
@@ -36,35 +40,39 @@ router.post('/register', (req, res) => {
   //FIXME: register new user and send response as JSON
   const newUser = req.body;
   let validEmail = true;
-  if (newUser.email && newUser.password && newUser.fullName && givenName && familyName && role) {
-    for (const user in usersArray) {
+  if (newUser.email && newUser.password && newUser.fullName && newUser.givenName && newUser.familyName && newUser.role) {
+    for (const user of usersArray) {
+      let i = 0;
       if (user.email == newUser.email) {
         validEmail = false;
         res.status(400).json({ error: 'Email already registered' });
         break;
       }
+      i++;
     }
     if (validEmail) {
-    usersArray.push(newUser);
-    res.status(200).json(newUser);
+      newUser.userId = nanoid();
+      newUser.creationTime = Date();
+      usersArray.push(newUser);
+      res.status(200).json({message: `New user ${newUser.fullName} registered!`});
     }
   } else {
-    if (!email) {
+    if (!newUser.email) {
       res.status(400).json({ error: 'Please enter an email' });
     }
-    if (!password) {
+    if (!newUser.password) {
       res.status(400).json({ error: 'Please enter a password' });
     }
-    if (!fullName) {
+    if (!newUser.fullName) {
       res.status(400).json({ error: 'Please enter your full name' });
     }
-    if (!givenName) {
+    if (!newUser.givenName) {
       res.status(400).json({ error: 'Please enter your given name' });
     }
-    if (!familyName) {
+    if (!newUser.familyName) {
       res.status(400).json({ error: 'Please enter your family name' });
     }
-    if (!role) {
+    if (!newUser.role) {
       res.status(400).json({ error: 'Please enter your role' });
     }
   }
@@ -72,14 +80,80 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   //FIXME: check user's email and password and send response as JSON
+  const loginAttempt = req.body;
+  if(loginAttempt.email && loginAttempt.password) {
+    let toLogin;
+    let validLogin = false;
+    for (const user of usersArray) {
+      if (user.email == loginAttempt.email && user.password == loginAttempt.password) {
+        toLogin = user;
+        res.status(200).json({message: `Welcome back ${toLogin.fullName}!`});
+        validLogin = true;
+        break;
+      }
+    }
+    if (!validLogin) {
+      res.status(404).json({ error: 'Invalid email or password. Please try again.' });
+    }
+  } else {
+    res.status(400).json({ error: 'Please enter an email and password.' });
+  }
 });
 
 router.put('/:userId', (req, res) => {
   //FIXME: update existing user and send response as JSON
+  let idFound = false;
+  let toUpdate;
+  for (const user of usersArray) {
+    if (user.userId == req.params.userId) {
+      toUpdate = user;
+      idFound = true;
+      break;
+    }
+  }
+  if (idFound) {
+    if (req.body.email) {
+      toUpdate.email = req.body.email;
+    }
+    if (req.body.password) {
+      toUpdate.password = req.body.password;
+    }
+    if (req.body.fullName) {
+      toUpdate.fullName = req.body.fullName;
+    }
+    if (req.body.givenName) {
+      toUpdate.givenName = req.body.givenName;
+    }
+    if (req.body.familyName) {
+      toUpdate.familyName = req.body.familyName;
+    }
+    if (req.body.role) {
+      toUpdate.role = req.body.role;
+    }
+    toUpdate.lastUpdated = Date();
+    res.status(200).json({message: `User ${toUpdate.fullName} updated!`});
+  } else {
+    res.status(404).json({ error: `User #${req.params.userId} not found.`});
+  }
 });
-
+    
 router.delete('/:userId', (req, res) => {
   //FIXME: delete user and send response as JSON
+  let idFound = false;
+  let toDelete;
+  for (const user of usersArray) {
+    if (user.userId == req.params.userId) {
+      toDelete = user;
+      idFound = true;
+      break;
+    }
+  }
+  if (!idFound) {
+  res.json({ error: `User #${req.params.userId} not found.` });
+  } else {
+    usersArray.splice(usersArray.indexOf(toDelete), 1);
+    res.status(200).json({message: `User ${toDelete.fullName} deleted!`});
+  }
 });
 
 export { router as userRouter };
